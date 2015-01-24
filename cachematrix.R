1f87503b0196630e9f72759db0e245e3eb859cda
Niko Visnjic <visnjic.niko@gmail.com>
2015-01-24 10:53:48 +0800

    up and running

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## RProg-010 assignment 2
+## Doing a bit of magic to construct a container for matrices
+## which can be used to recall previously computed inverted matrices
+## Pros: Less computing required! 
+## Cons: More RAM used for each "special" matrix container
 
-## Write a short comment describing this function
 
+## Matrix contrainer constructor with functions calls
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverz <- function(solve) m <<- solve
+    getinverz <- function() m
+    list(set = set, get = get,
+         setinverz = setinverz,
+         getinverz = getinverz)
 }
 
 
-## Write a short comment describing this function
+## Solver function with logic for checking if inverted matrix
+## already solved for specified input matrix container
+## Does not check value in other containers, only if the
+## provided container is solved previously
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getinverz()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverz(m)
+    m
+
 }
